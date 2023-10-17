59ffb6f42821ca46dade9c9c1dc00c03e837bd1f
fauziaNito <fauzianaava@gmail.com>
2023-10-17 16:00:41 +0300

    Initial commit

---
+++ server/controllers/user.js
@@ -0,0 +1,6 @@
+const test = (req, res) => {
+    console.log('Test is working');
+    res.json("This is just a Test")
+}
+
+export default test;
---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouters from './routes/users.js';
+import videoRouters from './routes/videos.js';
+import commentRouters from './routes/comments.js';
+import authRouters from './routes/auth.js';
+
+const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log('Connected to DB');
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+app.use(express.json());
+app.use('/api/users', userRouters);
+app.use('/api/videos', videoRouters);
+app.use('/api/comments', commentRouters);
+app.use('/api/auth', authRouters);
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || 'Something went wrong!';
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  connect();
+  console.log('connected');
+});
