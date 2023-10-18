268f5fc83ceb4b6cb122dee9bb21cb550360044e
fauziaNito <fauzianaava@gmail.com>
2023-10-18 11:45:31 +0300

    Added user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,21 @@
 import express from 'express';
-import test from '../controllers/user.js';
+import test, { deleteUser, dislike, getUser, like, subcribe, unsubcribe, update } from '../controllers/user.js';
 
 const router = express.Router();
 
+// update user
+router.put("/:id", update)
 
+// delete user
+router.delete("/:id", deleteUser)
+// get a user
+router.get("/find/:id", getUser)
+// subscribe a user
+router.put("/sub/:id", subcribe)
+// unsubscribe a user
+router.put("/unsub/:id", unsubcribe)
+// like a video
+router.put("/like/:videoId", like)
+// dislike a video
+router.put("/dislike/:videoId", dislike)
 export default router;
