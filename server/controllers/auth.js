d4f0701bff4575892bf148ef035de61e7ba8bf9c
fauziaNito <fauzianaava@gmail.com>
2023-10-18 11:41:58 +0300

    Added jwt for authorization to the signin function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import { createError } from '../error.js';
 
 export const signup = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password);
+    const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
@@ -21,9 +22,19 @@ export const signin = async (req, res, next) => {
     const user = await User.findOne({ name: req.body.name });
     if (!user) return next(createError(404, 'User not found!'));
 
-    const isCorrect = bcrypt.compare(req.body.password, user.password);
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
 
     if (!isCorrect) return next(createError(400, 'Wrong credentials'));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const {password, ...others} = user._doc
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
   } catch (error) {
     next(error);
   }
