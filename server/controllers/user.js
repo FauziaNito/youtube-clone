11cdb853817c49aea4031f71f38823f43ee61e21
fauziaNito <fauzianaava@gmail.com>
2023-10-18 11:44:44 +0300

    Added function boilerplates to  user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,21 @@
-const test = (req, res) => {
-    console.log('Test is working');
-    res.json("This is just a Test")
-}
+export const update = (req, res, next) => {
 
-export default test;
+}
+export const deleteUser = (req, res, next) => {
+
+}
+export const getUser = (req, res, next) => {
+
+}
+export const subcribe = (req, res, next) => {
+
+}
+export const unsubcribe = (req, res, next) => {
+
+}
+export const like = (req, res, next) => {
+
+}
+export const dislike = (req, res, next) => {
+
+}
