1bf871326569ce77c30ea309f07219edb3761840
fauziaNito <fauzianaava@gmail.com>
2023-10-19 09:24:33 +0300

    Added CRUD Video routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import {} from '../controllers/video.js';
+import verifyToken from "../verifyToken.js"
 
 const router = express.Router();
 
-
+// create video
+router.post("/", verifyToken, addVideo )
+router.put("/:id", verifyToken, updateVideo )
+router.delete("/:id", verifyToken, deleteVideo )
+router.get("/find/:id", addVideo, getVideo )
 
 export default router;
