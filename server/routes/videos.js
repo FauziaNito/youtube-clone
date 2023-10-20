7fb00032f5e5edfc317ae6ea4ca3986431ffe3dd
fauziaNito <fauzianaava@gmail.com>
2023-10-20 16:22:57 +0300

    Added more video routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import {} from '../controllers/video.js';
-import verifyToken from "../verifyToken.js"
+import { addVideo, addView, deleteVideo, getByTag, getVideo, random, search, sub, trend, updateVideo } from '../controllers/video.js';
+import {verifyToken} from "../verifyToken.js"
 
 const router = express.Router();
 
@@ -8,6 +8,12 @@ const router = express.Router();
 router.post("/", verifyToken, addVideo )
 router.put("/:id", verifyToken, updateVideo )
 router.delete("/:id", verifyToken, deleteVideo )
-router.get("/find/:id", addVideo, getVideo )
+router.get("/find/:id", getVideo )
+router.put("/view/:id", addView )
+router.get("/trend", trend )
+router.get("/random", random )
+router.get("/sub", sub )
+router.get('/tags', getByTag);
+router.get('/search', search);
 
 export default router;
